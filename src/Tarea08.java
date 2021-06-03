6c2fa9d2d7cfe04caa3c1ba040a589851e811b4d
roberto <aluper5445@ieselcaminas.org>
2021-06-03 18:31:53 +0200

    Initial commit

---
+++ src/Tarea08.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Tarea08 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array=new ArrayList<Integer>();
+        Random generador=new Random();
+        for (int i = 0; i<=9;i++){
+            int numeroAleatorio=generador.nextInt(100)+1;
+            array.add(numeroAleatorio);
+
+        }
+        System.out.println("El contenido del ArrayList original es: "+array);
+            int numeroMaximo= array.get(0);
+        for (int i = 0; i<array.size()-1;i++){
+            if (numeroMaximo< array.get(i)){
+                numeroMaximo= array.get(i);
+            }
+        }
+        System.out.println("El número más alto dentro del ArrayList es "+numeroMaximo);
+    }
+}
